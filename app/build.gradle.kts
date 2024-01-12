61e99029f6e4bbed42faac9938f38d71430278cf
qjatn0521 <qjatn0521@kw.ac.kr>
2024-01-13 00:10:31 +0900

    [Design] My frame design
    마이 화면 뼈대 완성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 
+    // Circle ImageView
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
 
 }
--- app/src/main/java/com/gst/gusto/MyFragment.kt
+++ app/src/main/java/com/gst/gusto/MyFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import com.gst.gusto.databinding.FragmentMyBinding
+import com.gst.gusto.my.MyViewpagerAdapter
 
 class MyFragment : Fragment() {
 
@@ -16,9 +19,34 @@ class MyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMyBinding.inflate(inflater, container, false)
-
+        initViewPager()
         return binding.root
 
+    }
+    private fun initViewPager() {
+        //ViewPager2 Adapter 셋팅
+        var viewPager2Adatper = MyViewpagerAdapter(requireActivity())
+        viewPager2Adatper.addFragment(MyReviewFragment())
+        viewPager2Adatper.addFragment(MyListFragment())
+
+        //Adapter 연결
+        binding.viewpager.apply {
+            adapter = viewPager2Adatper
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                }
+            })
+        }
+
+        //ViewPager, TabLayout 연결
+        TabLayoutMediator(binding.tablayout, binding.viewpager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "리뷰"
+                1 -> tab.text = "찜 리스트"
+            }
+        }.attach()
     }
 
 }
