5861d374a6aeaa1c96ee5a41d912f480d4ff2d94
mindddy <1117minsky@hufs.ac.kr>
2024-01-31 09:35:09 +0900

    Feat : store 상세 페이지 기본 클릭 리스너 구현

--- app/src/main/java/com/gst/gusto/ListView/Model/Store.kt
+++ app/src/main/java/com/gst/gusto/ListView/Model/Store.kt
@@ -1,5 +1,8 @@
 package com.gst.gusto.ListView.Model
 
+import android.app.appsearch.StorageInfo
+import android.health.connect.datatypes.StepsCadenceRecord
+
 data class Store(
     var id : Int,
     var storeName : String,
@@ -8,4 +11,27 @@ data class Store(
     var serverCategory : String? = null,
     var storePhoto : Int?,
     var isSaved : Boolean? = null
+)
+
+data class StoreDetailReview(
+    var reviewId : Int,
+    var visitedAt : String,
+    var nickname : String,
+    var liked : Int,
+    var comment : String,
+    var hashTageName : ArrayList<String>
+)
+
+data class StoreDetail(
+    var storeId : Int,
+    var storeName : String,
+    var categoryName : String,
+    var address : String,
+    var opening : Int,
+    var pin : Int,
+    var reviewImg : ArrayList<Int>,
+    var reviews : ArrayList<StoreDetailReview>,
+    var listSize : Int? = 0,
+    var totalPage : Int? = 0,
+    var totalElements : Int? = 0
 )
