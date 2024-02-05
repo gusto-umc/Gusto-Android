8db61d77b891827632b7e35cf01b03c232107982
yein <inwon9892@naver.com>
2024-02-06 00:12:48 +0900

    [Feat] Feed Tab Add

---
+++ app/src/main/java/com/gst/gusto/feed/FeedFragment.kt
@@ -0,0 +1,52 @@
+package com.gst.gusto.feed
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import com.gst.gusto.R
+import com.gst.gusto.databinding.FragmentFeedBinding
+import com.gst.gusto.review.adapter.GalleryReviewAdapter
+
+class FeedFragment : Fragment() {
+
+    lateinit var binding: FragmentFeedBinding
+    lateinit var adapter: GalleryReviewAdapter
+
+    // 테스트 이미지의 id
+    val testImageList = arrayOf(
+        R.drawable.review_gallery_test, R.drawable.review_gallery_test2, R.drawable.review_gallery_test,
+        R.drawable.review_gallery_test, R.drawable.review_gallery_test, R.drawable.review_gallery_test2,
+        R.drawable.review_gallery_test2, R.drawable.review_gallery_test, R.drawable.review_gallery_test,
+        R.drawable.review_gallery_test, R.drawable.review_gallery_test2, R.drawable.review_gallery_test2,
+        R.drawable.review_gallery_test2, R.drawable.review_gallery_test, R.drawable.review_gallery_test2,
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFeedBinding.inflate(inflater, container, false)
+
+        // 클릭 리스너 부분
+        adapter = GalleryReviewAdapter(testImageList, context,
+            itemClickListener = {
+                    it -> Toast.makeText(context,"테스트용", Toast.LENGTH_SHORT).show()
+            })
+
+        binding.apply {
+            recyclerView.adapter = adapter
+            recyclerView.layoutManager = GridLayoutManager(activity, 3)
+            adapter.notifyDataSetChanged()
+        }
+
+        return binding.root
+
+    }
+
+}
