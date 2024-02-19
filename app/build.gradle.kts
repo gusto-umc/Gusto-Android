1993db04b050f315f2f98e159ef02131858460c9
qjatn0521 <qjatn0521@kw.ac.kr>
2024-02-19 13:28:10 +0900

    [Fix] hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gst.gusto"
-        minSdk = 27
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
