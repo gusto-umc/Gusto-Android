0674ebc9d052e1e75ebc2eb7848e1763e9fbc86d
mindddy <1117minsky@hufs.ac.kr>
2024-02-20 00:27:44 +0900

    Fix : review detail 레이아웃 수정

--- app/src/main/java/com/gst/gusto/store_detail/StoreDetailFragment.kt
+++ app/src/main/java/com/gst/gusto/store_detail/StoreDetailFragment.kt
@@ -52,11 +52,71 @@ class StoreDetailFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_store_detail, container, false)
         return binding.root
     }
+    var pinId : Int? = null
+    val mReviewAdapter = StoreDetailReviewAdapter()
+    fun setDatas(data : ResponseStoreDetail?){
+        if(data != null){
+            binding.tvStoreDetailCategory.text = data!!.categoryString
+            binding.tvStoreDetailName.text = data!!.storeName
+            binding.tvStoreDetailAddress.text = data!!.address
+            pinId = data!!.pinId
+            if (data.pin){
+                binding.ivStoreDetailSave.setImageResource(R.drawable.save_o_img)
+            }
+            //리뷰사진 리사이클러뷰 연결
+            val mStorePhotos = ArrayList<String>().apply {
+                for(i in data.reviewImg4){
+                    this.add(i)
+                }
+            }
+            val mStorePhotoAdapter = StoreDetailPhotoAdapter(mStorePhotos)
+            mStorePhotoAdapter.mContext = context
+            binding.rvStoreDetailPhoto.adapter = mStorePhotoAdapter
+            //배너 사진 연결
+            if(!data.reviewImg4.isNullOrEmpty()) {
+                setImage(binding.ivStoreDetailBanner,data.reviewImg4[0], requireContext())
+            }
+            else{
+                binding.ivStoreDetailBanner.setImageResource(R.drawable.gst_dummypic)
+            }
+            //리뷰 리사이클러뷰 연결
+            mReviewAdapter.mContext = context
+            mReviewAdapter.setItemClickListener(object : StoreDetailReviewAdapter.OnItemClickListener{
+                override fun onClick(v: View, dataSet: ResponseReviews) {
+                    //데이터 넣기
+                    val bundle = Bundle()
+                    bundle.putLong("reviewId",dataSet.reviewId)
+                    bundle.putString("reviewNickname", dataSet.nickname)//리뷰 아이디 넘겨 주면 됨
+                    if(dataSet.nickname == gustoViewModel.userNickname){
+                        // 내 리뷰인 경우
+                        Navigation.findNavController(view!!).navigate(R.id.action_storeDetailFragment_to_fragment_review_detail, bundle)
+                    }
+                    else{
+                        //타 유저 리뷰인 경우
+                        gustoViewModel.currentFeedReviewId = dataSet.reviewId
+                        Log.d("feedId checking", "dataSet : ${dataSet.reviewId}, currentFeedReviewId : ${gustoViewModel.currentFeedReviewId}")
+                        gustoViewModel.getFeedReview{ result ->
+                            when(result) {
+                                1 -> {
+                                    findNavController().navigate(R.id.action_storeDetailFragment_to_fragment_feed_review_detail)
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+            })
+            mReviewAdapter.submitList(gustoViewModel.storeDetailReviews)
+            binding.rvStoreDetailReview.adapter = mReviewAdapter
+            binding.rvStoreDetailReview.layoutManager = LinearLayoutManager(this.requireActivity())
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var sampleStoreId = gustoViewModel.selectedDetailStoreId
-        var pinId : Int? = null
+
         gustoViewModel.detailReviewLastId = null
         gustoViewModel.detailReviewLastVisitedAt = null
         gustoViewModel.storeDetailReviews.clear()
@@ -65,66 +125,9 @@ class StoreDetailFragment : Fragment() {
         /**
          * 데이터 적용
          */
-        val mReviewAdapter = StoreDetailReviewAdapter()
 
-        fun setDatas(data : ResponseStoreDetail?){
-            if(data != null){
-                binding.tvStoreDetailCategory.text = data!!.categoryString
-                binding.tvStoreDetailName.text = data!!.storeName
-                binding.tvStoreDetailAddress.text = data!!.address
-                pinId = data!!.pinId
-                if (data.pin){
-                    binding.ivStoreDetailSave.setImageResource(R.drawable.save_o_img)
-                }
-                //리뷰사진 리사이클러뷰 연결
-                val mStorePhotos = ArrayList<String>().apply {
-                    for(i in data.reviewImg4){
-                        this.add(i)
-                    }
-                }
-                val mStorePhotoAdapter = StoreDetailPhotoAdapter(mStorePhotos)
-                mStorePhotoAdapter.mContext = context
-                binding.rvStoreDetailPhoto.adapter = mStorePhotoAdapter
-                //배너 사진 연결
-                if(!data.reviewImg4.isNullOrEmpty()) {
-                    setImage(binding.ivStoreDetailBanner,data.reviewImg4[0], requireContext())
-                }
-                else{
-                    binding.ivStoreDetailBanner.setImageResource(R.drawable.gst_dummypic)
-                }
-                //리뷰 리사이클러뷰 연결
-                mReviewAdapter.mContext = context
-                mReviewAdapter.setItemClickListener(object : StoreDetailReviewAdapter.OnItemClickListener{
-                    override fun onClick(v: View, dataSet: ResponseReviews) {
-                        //데이터 넣기
-                        val bundle = Bundle()
-                        bundle.putLong("reviewId",dataSet.reviewId)
-                        bundle.putString("reviewNickname", dataSet.nickname)//리뷰 아이디 넘겨 주면 됨
-                        if(dataSet.nickname == gustoViewModel.userNickname){
-                            // 내 리뷰인 경우
-                            Navigation.findNavController(view).navigate(R.id.action_storeDetailFragment_to_fragment_review_detail, bundle)
-                        }
-                        else{
-                            //타 유저 리뷰인 경우
-                            gustoViewModel.currentFeedReviewId = dataSet.reviewId
-                            Log.d("feedId checking", "dataSet : ${dataSet.reviewId}, currentFeedReviewId : ${gustoViewModel.currentFeedReviewId}")
-                            gustoViewModel.getFeedReview{ result ->
-                                when(result) {
-                                    1 -> {
-                                        findNavController().navigate(R.id.action_storeDetailFragment_to_fragment_feed_review_detail)
-                                    }
-                                }
-                            }
-                        }
 
-                    }
 
-                })
-                mReviewAdapter.submitList(gustoViewModel.storeDetailReviews)
-                binding.rvStoreDetailReview.adapter = mReviewAdapter
-                binding.rvStoreDetailReview.layoutManager = LinearLayoutManager(this.requireActivity())
-            }
-        }
 
         fun loadReviews(reviews : ArrayList<ResponseReviews>){
             mReviewAdapter.submitList(gustoViewModel.storeDetailReviews)
@@ -250,5 +253,10 @@ class StoreDetailFragment : Fragment() {
         }
 
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setDatas(gustoViewModel.myStoreDetail)
     }
 }
