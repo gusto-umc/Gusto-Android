1a2ad722f7ee4ec8bebd295e316743b595fff8d0
yein <inwon9892@naver.com>
2024-05-04 02:07:28 +0900

    [Add] #222 insta dto, model 세팅

--- app/src/main/java/com/gst/gusto/dto/InstaReviewDtoSet.kt
+++ app/src/main/java/com/gst/gusto/dto/InstaReviewDtoSet.kt
@@ -1,16 +1,33 @@
 package com.gst.gusto.dto
 
 import com.google.gson.annotations.SerializedName
+import com.gst.gusto.model.InstaReview
+import com.gst.gusto.model.InstaReviews
 
 // insta (gallery) review 조회- reviews & MyReviewFragment와 OtherReviewFragment
 data class ResponseInstaReview(
     @SerializedName("reviewId") val reviewId: Long,
     @SerializedName("images") val image: String,
-)
+) {
+    fun toDomainModel(): InstaReview {
+        return InstaReview(
+            reviewId = reviewId,
+            image = image
+        )
+    }
+}
 
 // insta (gallery) review 조회 & MyReviewFragment와 OtherReviewFragment
 data class ResponseInstaReviews(
     @SerializedName("reviews") val reviews: List<ResponseInstaReview>,
     @SerializedName("hasNext") val hasNext: Boolean,
     @SerializedName("cursorId") val cursorId: Long,
-)
+) {
+    fun toDomainModel(): InstaReviews {
+        return InstaReviews(
+            reviews = reviews.map { it.toDomainModel() },
+            hasNext = hasNext,
+            cursorId = cursorId
+        )
+    }
+}
--- app/src/main/java/com/gst/gusto/model/InstaReviewData.kt
+++
@@ -1,8 +0,0 @@
-package com.gst.gusto.model
-
-import com.google.gson.annotations.SerializedName
-
-data class InstaReviewData(
-    val reviewId: Long = -1,
-    val image: String = ""
-)
