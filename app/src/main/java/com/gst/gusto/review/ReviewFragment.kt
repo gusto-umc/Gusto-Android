b5be4fa7c0fbdc7e7e9c65311018bea2346c452b
yein <inwon9892@naver.com>
2024-07-15 04:37:58 +0900

    [Feat] #267 캘린더리뷰 디자인 수정 및 기능 구현

--- app/src/main/java/com/gst/gusto/review/ReviewFragment.kt
+++ app/src/main/java/com/gst/gusto/review/ReviewFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import com.gst.gusto.R
 import com.gst.gusto.api.GustoViewModel
 import com.gst.gusto.databinding.FragmentReviewBinding
+import com.gst.gusto.review.fragment.CalendarReviewFragment
 import com.gst.gusto.review.fragment.InstaReviewFragment
 import com.gst.gusto.review.viewmodel.InstaReviewViewModel
 import com.gst.gusto.review.viewmodel.InstaReviewViewModelFactory
@@ -17,8 +18,6 @@ import com.gst.gusto.review.viewmodel.InstaReviewViewModelFactory
 class ReviewFragment : Fragment() {
 
     lateinit var binding: FragmentReviewBinding
-
-    val icons = listOf(R.drawable.gallery_review_img, R.drawable.calendar_review_img, R.drawable.list_review_img)
 
     private val gustoViewModel : GustoViewModel by activityViewModels()
     private val instaViewModel: InstaReviewViewModel by viewModels{ InstaReviewViewModelFactory() }
@@ -35,43 +34,15 @@ class ReviewFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // initViewPager()
         setFragment()
     }
 
     private fun setFragment() {
         val fragmentMananger = childFragmentManager.beginTransaction()
-        fragmentMananger.replace(R.id.review_fragment, InstaReviewFragment())
+        // fragmentMananger.replace(R.id.review_fragment, InstaReviewFragment())
+        fragmentMananger.replace(R.id.review_fragment, CalendarReviewFragment())
         fragmentMananger.commit()
 
     }
-
-    /*private fun initViewPager() {
-        //ViewPager2 Adapter 셋팅
-        var VPAdapter = ReviewAdapter(this)
-        VPAdapter.addFragment(InstaReviewFragment())
-        VPAdapter.addFragment(CalendarReviewFragment())
-        VPAdapter.addFragment(ListReviewFragment())
-
-        //Adapter 연결
-        binding.reviewVP.apply {
-            adapter = VPAdapter
-
-        }
-
-        // ViewPager TabLayout 연결
-        TabLayoutMediator(binding.reviewTab, binding.reviewVP) { tab, position ->
-            tab.setIcon(icons[position])
-        }.attach()
-
-        // 각 탭에 OnClickListener 설정
-        for (i in 0 until binding.reviewTab.tabCount) {
-            val tab = binding.reviewTab.getTabAt(i)
-            tab?.view?.setOnClickListener {
-                binding.reviewVP.currentItem = i
-            }
-        }
-
-    }*/
 
 }
