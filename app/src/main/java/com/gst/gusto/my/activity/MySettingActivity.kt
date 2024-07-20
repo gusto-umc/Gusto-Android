7ee3d03da80383685a14c23b704c43adf0446d32
강예인 <yein@gang-yein-ui-MacBookAir.local>
2024-07-21 00:23:47 +0900

    [Feat] #272 나의 콘텐츠 공개 여부 get 데이터 연결

--- app/src/main/java/com/gst/gusto/my/activity/MySettingActivity.kt
+++ app/src/main/java/com/gst/gusto/my/activity/MySettingActivity.kt
@@ -4,17 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.Scope
-import com.gst.gusto.MainActivity
-import com.gst.gusto.R
 import com.gst.gusto.api.GustoViewModel
 import com.gst.gusto.databinding.ActivityMySettingBinding
+import com.gst.gusto.my.viewmodel.MySettingViewModel
+import com.gst.gusto.my.viewmodel.MySettingViewModelFactory
 import com.gst.gusto.start.StartActivity
 import com.gst.gusto.util.GustoApplication
 import com.kakao.sdk.user.UserApiClient
@@ -27,6 +25,8 @@ class MySettingActivity : AppCompatActivity() {
     lateinit var binding: ActivityMySettingBinding
     private val gustoViewModel : GustoViewModel by viewModels()
 
+    private val mySettingViewModel: MySettingViewModel by viewModels { MySettingViewModelFactory() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMySettingBinding.inflate(layoutInflater)
@@ -34,6 +34,7 @@ class MySettingActivity : AppCompatActivity() {
         buttonSetting()
         gustoViewModel.getTokens()
         getPublishData()
+        setToast()
         setContentView(binding.root)
         setReviewButton()
     }
@@ -110,15 +111,12 @@ class MySettingActivity : AppCompatActivity() {
     }
 
     fun getPublishData() {
-        gustoViewModel.myPublishGet { result, response ->
-            if(result == 1) {
-                if(response != null) {
-                    Log.d("publish", response.toString())
-                    gustoViewModel.myPublishData.observe(this, Observer { value ->
-                        binding.reviewSwitch.isChecked = value?.publishReview ?: false
-                        binding.jimSwitch.isChecked = value?.publishPin ?: false
-                    })
-                }
+
+        mySettingViewModel.myPublishData.observe(this){
+            with(binding){
+                reviewSwitch.isChecked = it?.publishReview ?: false
+                pinSwitch.isChecked = it?.publishPin ?: false
+                routeSwitch.isChecked = it?.publishRoute ?: false
             }
         }
     }
@@ -126,7 +124,7 @@ class MySettingActivity : AppCompatActivity() {
     fun setPublishData() {
         with(binding) {
             val reviewSwitch = this.reviewSwitch.isChecked
-            val pinSwitch = this.jimSwitch.isChecked
+            val pinSwitch = this.pinSwitch.isChecked
             gustoViewModel.myPublishSet(reviewSwitch,pinSwitch) {result, ->
             when(result) {
                 1 -> {
@@ -199,6 +197,15 @@ class MySettingActivity : AppCompatActivity() {
             }
             else {
             }
+        }
+    }
+
+    private fun setToast(){
+        mySettingViewModel.tokenToastData.observe(this){
+            Toast.makeText(this, "토큰을 재 발급 중입니다", Toast.LENGTH_SHORT).show()
+        }
+        mySettingViewModel.errorToastData.observe(this){
+            Toast.makeText(this, "서버와의 연결 불안정", Toast.LENGTH_SHORT).show()
         }
     }
 }
