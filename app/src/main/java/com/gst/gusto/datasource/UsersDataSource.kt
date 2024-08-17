e12c18c9fb07109dc8e7c3117b516d653d61b278
yein <inwon9892@naver.com>
2024-08-18 02:03:44 +0900

    [Feat] #218 프로필 정보 조회 API 추가

--- app/src/main/java/com/gst/gusto/datasource/UsersDataSource.kt
+++ app/src/main/java/com/gst/gusto/datasource/UsersDataSource.kt
@@ -3,8 +3,10 @@ package com.gst.gusto.datasource
 import com.gst.gusto.api.ApiResponse
 import com.gst.gusto.api.service.UsersApi
 import com.gst.gusto.dto.RequestMyPublish
+import com.gst.gusto.dto.ResponseMyProfile
 import com.gst.gusto.dto.ResponseMyPublish
 import okhttp3.ResponseBody
+import retrofit2.http.Header
 
 class UsersDataSource(
     private val usersService: UsersApi
@@ -31,6 +33,19 @@ class UsersDataSource(
             response.body()?.let{
                 ApiResponse.Success(it)
             } ?: ApiResponse.Error(response.code(), "Response body is null + ${response.message()}")
+        } else {
+            ApiResponse.Error(response.code(), response.message())
+        }
+    }
+
+    suspend fun getMyProfile(
+        token: String
+    ): ApiResponse<ResponseMyProfile> {
+        val response = usersService.getMyProfile(token)
+        return if(response.isSuccessful){
+            response.body()?.let{
+                ApiResponse.Success(it)
+            } ?: ApiResponse.Error(response.code(), "Response body is null + ${response.message()}" )
         } else {
             ApiResponse.Error(response.code(), response.message())
         }
--- app/src/main/java/com/gst/gusto/repository/UsersRepositoryImpl.kt
+++ app/src/main/java/com/gst/gusto/repository/UsersRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.gst.gusto.repository
 import com.gst.gusto.api.ApiResponse
 import com.gst.gusto.datasource.UsersDataSource
 import com.gst.gusto.dto.toRequestModel
+import com.gst.gusto.model.MyProfileData
 import com.gst.gusto.model.MyPublishData
 import okhttp3.ResponseBody
 
@@ -22,6 +23,15 @@ class UsersRepositoryImpl(
     ): ApiResponse<ResponseBody> {
         return when(val response = usersDataSource.setMyPublish(token, myPublish.toRequestModel())){
             is ApiResponse.Success -> ApiResponse.Success(response.data)
+            is ApiResponse.Error -> ApiResponse.Error(response.errorCode, response.errorMessage)
+        }
+    }
+
+    override suspend fun getMyProfile(
+        token: String
+    ): ApiResponse<MyProfileData> {
+        return when(val response = usersDataSource.getMyProfile(token)) {
+            is ApiResponse.Success -> ApiResponse.Success(response.data.toDomainModel())
             is ApiResponse.Error -> ApiResponse.Error(response.errorCode, response.errorMessage)
         }
     }
