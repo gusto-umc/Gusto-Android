7ec9355c0a0888bf20c48b1fec19abdabc9fadd0
yein <inwon9892@naver.com>
2024-08-20 16:41:54 +0900

    [Feat] #291 프로필 정보 수정 기능 추가

--- app/src/main/java/com/gst/gusto/my/activity/MyProfileEditActivity.kt
+++ app/src/main/java/com/gst/gusto/my/activity/MyProfileEditActivity.kt
@@ -1,13 +1,27 @@
 package com.gst.gusto.my.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ext.SdkExtensions
+import android.provider.Settings
+import android.util.Log
 import android.view.ContextThemeWrapper
 import android.view.View
+import android.widget.ImageView
 import android.widget.PopupMenu
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.gst.gusto.R
 import com.gst.gusto.databinding.ActivityMyProfileEditBinding
 import com.gst.gusto.my.Age
@@ -15,7 +29,9 @@ import com.gst.gusto.my.Gender
 import com.gst.gusto.my.fragment.MyProfileImageEditBottomSheet
 import com.gst.gusto.my.viewmodel.MyProfileEditViewModel
 import com.gst.gusto.my.viewmodel.MyProfileEditViewModelFactory
+import com.gst.gusto.util.util.Companion.convertContentToFile
 import com.gst.gusto.util.util.Companion.setImage
+import com.gst.gusto.util.util.Companion.toAbsolutePath
 
 // MyProfileFragment 참고해서 디자인
 
@@ -24,25 +40,71 @@ class MyProfileEditActivity : AppCompatActivity() {
     lateinit var binding: ActivityMyProfileEditBinding
 
     private val viewModel: MyProfileEditViewModel by viewModels{ MyProfileEditViewModelFactory() }
+
+    private lateinit var pickMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private lateinit var albumLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMyProfileEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            uri?.let { drawImage(binding.profileImageView, it) }
+        }
+
+        albumLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val uri = result.data?.data
+                uri?.let { drawImage(binding.profileImageView, uri) }
+            }
+        }
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    startAlbumLauncher()
+                } else {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val packageName = this.packageName
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+            }
 
         with(binding) {
             btnBack.setOnClickListener{
                 finish()
             }
             saveBtn.setOnClickListener {
-                finish()
+                viewModel.setProfile()
             }
+
+            viewModel.setProfileData.observe(this@MyProfileEditActivity){
+                if(it){
+                    setResult(RESULT_OK)
+                    finish()
+                }
+            }
+
             changeProfileImageBtn.setOnClickListener {
                 MyProfileImageEditBottomSheet(
                     onBasicProfileClickListener = {
                         profileImageView.setImageResource(R.drawable.gst_dummypic)
+                        viewModel.setDefaultProfileImg()
                     },
                     onProfileClickListener = {
-
+                        if (isPhotoPickerAvailable()) {
+                            this@MyProfileEditActivity.pickMedia.launch(
+                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            )
+                        } else {
+                            checkPermission()
+                        }
                     }
                 ).show(supportFragmentManager, "MyProfileImageEditBottomSheet")
             }
@@ -57,6 +119,48 @@ class MyProfileEditActivity : AppCompatActivity() {
 
         getProfileInfo()
         checkNickName()
+    }
+
+    private fun drawImage(view: ImageView, imgUrl: Uri) {
+        Glide.with(this)
+            .load(imgUrl)
+            .into(view)
+
+        // val path = this.toAbsolutePath(imgUrl)
+        val path = convertContentToFile(this@MyProfileEditActivity, imgUrl).absolutePath.toString()
+        viewModel.setProfileImg(path)
+    }
+
+    private fun checkPermission() {
+        val permissionReadExternal = android.Manifest.permission.READ_EXTERNAL_STORAGE
+
+        val permissionReadExternalGranted = ContextCompat.checkSelfPermission(
+            this,
+            permissionReadExternal
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (permissionReadExternalGranted) {
+            startAlbumLauncher()
+        } else {
+            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun startAlbumLauncher() {
+        val albumIntent = Intent(Intent.ACTION_PICK)
+        albumIntent.type = "image/*"
+        albumLauncher.launch(albumIntent)
+    }
+
+    private fun isPhotoPickerAvailable(): Boolean {
+        return when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> true
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> SdkExtensions.getExtensionVersion(
+                Build.VERSION_CODES.R
+            ) >= 2
+
+            else -> false
+        }
     }
 
     private fun getProfileInfo() {
@@ -154,6 +258,7 @@ class MyProfileEditActivity : AppCompatActivity() {
         viewModel.checkNickNameData.observe(this@MyProfileEditActivity){
             with(binding){
                 if(it){
+                    viewModel.setNickname(nicknameEditText.text.toString())
                     checkBtn.visibility = View.GONE
                     checkIcon.visibility = View.VISIBLE
                     nicknameCheck.visibility = View.VISIBLE
